dd90256a5a79d4f59a2384ba2499f5404482be8f
dibin666 <ddddd2468hjk@gmail.com>
2023-05-12 23:22:46 +0800

    update

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# AnyKernel3 路径
+ANYKERNEL3_DIR=$PWD/AnyKernel3/
+# 编译完成后内核名字
+FINAL_KERNEL_ZIP=perf_KernelSU_A13_dibin.zip
+# 内核工作目录
+export KERNEL_DIR=$(pwd)
+# 内核 defconfig 文件
+export KERNEL_DEFCONFIG=loire_kugo_defconfig
+# 编译临时目录，避免污染根目录
+export OUT=out
+# clang 绝对路径
+export CLANG_PATH=/mnt/disk/tool/kugo/clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=${CLANG_PATH}/bin/aarch64-linux-gnu-
+# gcc 绝对路径
+export GCC_PATH=/mnt/disk/tool/kugo/gcc4.9
+export PATH=${GCC_PATH}/bin:${PATH}
+# arch平台，这里时arm64
+export ARCH=arm64
+#export SUBARCH=arm64
+# 只使用clang编译需要配置
+export LLVM=1
+export BUILD_INITRAMFS=1
+
+# ./build.sh 4
+
+#16为线程数，可以指定#
+TH_COUNT=16
+if [[ "" != "$1" ]]; then
+        TH_NUM=$1
+fi
+
+export DEF_ARGS="O=${OUT} \
+                                CC=clang \
+				ARCH=${ARCH} \
+                                CROSS_COMPILE=${CLANG_PATH}/bin/aarch64-linux-gnu- \
+                                CROSS_COMPILE_ARM32=${CLANG_PATH}/bin/arm-linux-gnueabi- \
+				LD=ld.lld "
+
+export BUILD_ARGS="-j${TH_COUNT} ${DEF_ARGS}"
+
+echo -e "$yellow**** 开始编译内核 ****$nocol"
+make ${DEF_ARGS} ${KERNEL_DEFCONFIG}
+make ${BUILD_ARGS}
+
+echo -e "$yellow**** 验证 Image.gz-dtb ****$nocol"
+ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+echo -e "$yellow**** 进入 AnyKernel3 目录 ****$nocol"
+ls $ANYKERNEL3_DIR
+echo -e "$yellow**** 清理 AnyKernel3 目录 ****$nocol"
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+echo -e "$yellow**** 复制 Image.gz-dtb 到 AnyKernel3 目录 ****$nocol"
+cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+echo -e "$yellow**** 正在打包内核为可刷入 Zip 文件 ****$nocol"
+cd $ANYKERNEL3_DIR/
+zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
+echo -e "$yellow**** 复制打包好的 Zip 文件到指定的目录 ****$nocol"
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /mnt/disk/kernelout
+echo -e "$yellow**** 清理目录 ****$nocol"
+cd ..
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf out/
+echo -e "$yellow**** 构建完成 ****$nocol"
